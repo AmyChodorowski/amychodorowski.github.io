e4235d3d15c931c7416a26256ed377263d2fcb0a
Amy <amychods@live.com>
2020-05-05 17:24:20 +0100

    Attempting to run Pytho code via transcrypt

---
+++ examples/__javascript__/hello.mod.js
@@ -0,0 +1,14 @@
+	(function () {
+		var greet = function () {
+			var py_name = document.getElementById ('Name').value;
+			if (py_name == '' || py_name.length == 0 || py_name == null) {
+				document.getElementById ('groet').innerHTML = '<p><font color="#ff0000">Hello Anonymous, may I know yor name? Please insert it below:</font></p>';
+			}
+			else {
+				document.getElementById ('groet').innerHTML = ('<p><font color="#00ff00">Hello, ' + py_name) + ', thank you for introducing you</font></p>';
+			}
+		};
+		__pragma__ ('<all>')
+			__all__.greet = greet;
+		__pragma__ ('</all>')
+	}) ();
