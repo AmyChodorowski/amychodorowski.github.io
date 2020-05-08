51bf2c998e8abda5e601b763381a327bb2b6541a
Amy <amychods@live.com>
2020-05-08 13:13:59 +0100

    Removing transcrypt generated files

--- transcrypt/__target__/org.transcrypt.__runtime__.js
+++ transcrypt/__target__/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2020-05-08 11:37:03
+// Transcrypt'ed from Python, 2020-05-08 13:12:50
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
